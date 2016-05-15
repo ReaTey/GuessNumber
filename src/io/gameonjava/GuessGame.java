5328dbe58c3821d7667a4e6d001060460debb6d7
reatey <andriy.gryniv@gmail.com>
2016-05-15 09:44:06 +0300

    Main class for starting the game.

---
+++ src/io/gameonjava/GuessGame.java
@@ -0,0 +1,13 @@
+package io.gameonjava;
+
+/**
+ * Created by reatey on 15.05.16.
+ */
+public class GuessGame {
+
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.startGame();
+    }
+
+}
