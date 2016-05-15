c3a4b98b8c43b56836e374fb1def3c70b9e108de
reatey <andriy.gryniv@gmail.com>
2016-05-15 09:50:17 +0300

    Main class for starting the game.

--- src/io/gameonjava/GuessGame.java
+++ src/io/gameonjava/GuessGame.java
@@ -3,8 +3,10 @@ package io.gameonjava;
 public class GuessGame {
 
     public static void main(String[] args) {
+
         GameController gameController = new GameController();
         gameController.startGame();
+
     }
 
 }
